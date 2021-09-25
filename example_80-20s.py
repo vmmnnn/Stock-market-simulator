8fdec5e2689f072d82b173b764212afcfd4e2fac
Marina <vmmnnn@mail.ru>
2021-09-25 17:13:08 +0300

    example algorithm divided into functions

--- example_80-20s.py
+++ example_80-20s.py
@@ -11,6 +11,55 @@ class MyAccountSimulator(AccountSimulator):
         self.tickers_to_sell = {}  # ticker -> price
         self.tickers_to_buy = {}   # ticker -> price
 
+    def __get_suitable_tickers(self, date, market):
+        yesterday = date - BDay(1)
+
+        self.tickers_to_sell = {}
+        self.tickers_to_buy = {}
+
+        for ticker in self.ticker:
+            yesterday_high = market.get_high_day_price(ticker, yesterday)
+            yesterday_low = market.get_low_day_price(ticker, yesterday)
+            yesterday_open = market.get_open_day_price(ticker, yesterday)
+            yesterday_close = market.get_close_day_price(ticker, yesterday)
+            range = yesterday_high - yesterday_low
+
+            if (yesterday_open <= 0.2 * range + yesterday_low) and (yesterday_close >= 0.8 * range + yesterday_low): # sell today
+                if self.get_quantity(ticker) > 0:
+                    self.tickers_to_sell[ticker] = yesterday_close
+            elif (yesterday_close <= 0.2 * range + yesterday_low) and (yesterday_open >= 0.8 * range + yesterday_low): # buy today
+                self.tickers_to_buy[ticker] = yesterday_close
+
+    def check_and_buy(self, market):
+        tickers_to_del = []
+        for ticker in self.tickers_to_buy.keys():
+            if market.get_current_price(ticker) <= self.tickers_to_buy[ticker]:
+                self.buy(ticker, 1)
+                tickers_to_del.append(ticker)
+        for ticker in tickers_to_del:
+            del self.tickers_to_buy[ticker]
+
+    def check_and_sell(self, market):
+        tickers_to_del = []
+        for ticker in self.tickers_to_sell.keys():
+            if market.get_current_price(ticker) >= self.tickers_to_sell[ticker]:
+                n = self.get_quantity(ticker)
+                self.sell(ticker, n)
+                tickers_to_del.append(ticker)
+        for ticker in tickers_to_del:
+            del self.tickers_to_sell[ticker]
+
+    def print_day_results(self, date):
+        print()
+        print(f"      {date.strftime('%Y-%m-%d')}: day is over")
+        print(f"portfolio costs {self.get_portfolio_cost():.2f} = {self.get_free_money():.2f} free money left + {self.get_active_money():.2f} stocks cost in total")
+        print("  portfolio:")
+        self.print_stocks()
+        print("  history:")
+        self.print_operations_history()
+        print()
+        sys.stdout.flush()
+
     # 80-20's algorithm:
     # If ('Open' of the day is in the lowest 20% of the range of this day) and
     #    ('Close' of the day is in the highest 80% of the range of this day) then
@@ -22,50 +71,14 @@ class MyAccountSimulator(AccountSimulator):
 
         # new day => define tickers that fit the 80-20 condition
         if date.hour == 9 and date.minute == 30:
-            self.tickers_to_sell = {}
-            self.tickers_to_buy = {}
-            for ticker in self.ticker:
-                yesterday = date - BDay(1)
-                yesterday_high = market.get_high_day_price(ticker, yesterday)
-                yesterday_low = market.get_low_day_price(ticker, yesterday)
-                yesterday_open = market.get_open_day_price(ticker, yesterday)
-                yesterday_close = market.get_close_day_price(ticker, yesterday)
-                range = yesterday_high - yesterday_low
-
-                if (yesterday_open <= 0.2 * range + yesterday_low) and (yesterday_close >= 0.8 * range + yesterday_low): # sell today
-                    if self.get_quantity(ticker) > 0:
-                        self.tickers_to_sell[ticker] = yesterday_close
-                elif (yesterday_close <= 0.2 * range + yesterday_low) and (yesterday_open >= 0.8 * range + yesterday_low): # buy today
-                    self.tickers_to_buy[ticker] = yesterday_close
+            self.__get_suitable_tickers(date, market)
             return
 
-        tickers_to_del = []
-        for ticker in self.tickers_to_buy.keys():
-            if market.get_current_price(ticker) <= self.tickers_to_buy[ticker]:
-                self.buy(ticker, 1)
-                tickers_to_del.append(ticker)
-        for ticker in tickers_to_del:
-            del self.tickers_to_buy[ticker]
-
-        tickers_to_del = []
-        for ticker in self.tickers_to_sell.keys():
-            if market.get_current_price(ticker) >= self.tickers_to_sell[ticker]:
-                n = self.get_quantity(ticker)
-                self.sell(ticker, n)
-                tickers_to_del.append(ticker)
-        for ticker in tickers_to_del:
-            del self.tickers_to_sell[ticker]
+        self.check_and_buy(market)
+        self.check_and_sell(market)
 
         if date.hour == 15 and date.minute == 30:
-            print()
-            print(f"      {date.strftime('%Y-%m-%d')}: day is over")
-            print(f"portfolio costs {self.get_portfolio_cost():.2f} = {self.get_free_money():.2f} free money left + {self.get_active_money():.2f} stocks cost in total")
-            print("  portfolio:")
-            self.print_stocks()
-            print("  history:")
-            self.print_operations_history()
-            print()
-            sys.stdout.flush()
+            self.print_day_results(date)
 
 
 start_date = datetime(year=2021, month=3, day=4)
